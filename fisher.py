86f17016bf1aaa93d00385198d4b83e9e92b8c97
sunguangquan <sunmanlyh@gmail.com>
2021-05-07 15:16:31 +0800

    config

--- fisher.py
+++ fisher.py
@@ -1,13 +1,13 @@
 from flask import Flask
-
-
+from config import DEBUG, HOST, PORT
 app = Flask(__name__)
 
 
+# app.add_url_rule('/', view_func=hello)
 @app.route('/hello')
 def hello():
     return 'Hello Flask!'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=DEBUG, host=HOST, port=PORT)
